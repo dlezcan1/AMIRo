a4612d3d3ca2d340d221fbb835466f3f031741c3
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-06-07 16:44:50 -0400

    Updated support functions and shape sensing repo changes.

--- src/matlab_scripts/readin_fbgparam_file.m
+++ src/matlab_scripts/readin_fbgparam_file.m
@@ -7,8 +7,10 @@
 function [kc, w_init, L, theta0] = readin_fbgparam_file(filename)
     tbl = readtable(filename);
     
+    tbl = mergevars(tbl, "w_init_" + (1:3), 'NewVariableName', 'w_init');
+    
     kc = tbl.kc(1);
-    w_init = reshape(tbl{1,2:end}, [], 1);
+    w_init = reshape(tbl.w_init, [], 1);
     theta0 = tbl.theta0(1);
     L = tbl.L(1);
     
