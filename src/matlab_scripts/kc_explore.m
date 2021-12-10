0b16a0a9f14b774f82c94407d7593ecf6e515b87
Dimitri Lezcano <dimitrilezcano@Dimitris-MacBook-Pro.local>
2021-12-10 16:42:32 -0500

    Updated kappac exlporation.

--- src/matlab_scripts/kc_explore.m
+++ src/matlab_scripts/kc_explore.m
@@ -1,6 +1,6 @@
 %% kc_explore.m
 % explore kappa c versus insertion depth
-data_dir = '../../data/3CH-4AA-0004/Insertion_Experiment_Results';
+data_dir = '/Volumes/amiro_needle_data/3CH-4AA-0004/Insertion_Experiment_Results';
 result_file = fullfile(data_dir, ...
     'FBG-Camera-Comp_tip-pcr_FBG-weights_combined-results.mat');
 act_result_tbl = load(result_file).act_result_tbl;
@@ -27,8 +27,8 @@ if ~isvariable(act_result_tbl, 'ShapeType')
 end
 
 %% Determine kc_ref and L_ref
-L_min = 30; % minimum reference insertion depth (for cut-off)
-mask_L_min = act_result_tbl.L >= L_min;
+L_min = 35; % minimum reference insertion depth (for cut-off)
+mask_L = act_result_tbl.L >= L_min;% & act_result_tbl.L <= 105;
 mask_c_1layer = act_result_tbl.ShapeType == "1-Layer C-Shape";
 mask_s_1layer = act_result_tbl.ShapeType == "1-Layer S-Shape";
 mask_c_2layer = act_result_tbl.ShapeType == "2-Layer C-Shape";
@@ -47,7 +47,7 @@ for expmt = unique(act_result_tbl.Experiment)'
         mask_ins_hole = act_result_tbl.Ins_Hole == ins_hole;
         
         % determine ref. for 1-layer C-shape
-        mask1 = mask_expmt & mask_ins_hole & mask_L_min & ...
+        mask1 = mask_expmt & mask_ins_hole & mask_L & ...
             mask_c_1layer;
         L_ref_c_1layer = min(act_result_tbl.L(mask1));
         if ~isempty(L_ref_c_1layer)
@@ -63,7 +63,7 @@ for expmt = unique(act_result_tbl.Experiment)'
         end
 
         % determine ref. for 1-layer S-shape
-        mask2 = mask_expmt & mask_ins_hole & mask_L_min & ...
+        mask2 = mask_expmt & mask_ins_hole & mask_L & ...
             mask_s_1layer;
         L_ref_s_1layer = min(act_result_tbl.L(mask2));
         if ~isempty(L_ref_s_1layer)
@@ -79,7 +79,7 @@ for expmt = unique(act_result_tbl.Experiment)'
         end
 
         % determine ref. for 2-layer C-shape
-        mask3 = mask_expmt & mask_ins_hole & mask_L_min & ...
+        mask3 = mask_expmt & mask_ins_hole & mask_L & ...
             mask_c_2layer;
         L_ref_c_2layer = min(act_result_tbl.L(mask3));
         if ~isempty(L_ref_c_2layer)
@@ -105,7 +105,7 @@ act_result_tbl.kc1_ratio = act_result_tbl.kc1 ./ act_result_tbl.kc1_ref .* ...
     sign(act_result_tbl.kc1);
 act_result_tbl.kc2_ratio = act_result_tbl.kc2 ./ act_result_tbl.kc2_ref .* ...
     sign(act_result_tbl.kc2);
-act_result_tbl.L_ratio = act_result_tbl.L./act_result_tbl.L_ref;
+act_result_tbl.L_ratio = act_result_tbl.L ./ act_result_tbl.L_ref;
 
 %% Plotting
 % plotting masks
@@ -120,10 +120,10 @@ mask_hard2 = (act_result_tbl.TissueStiffness2 > 29);
 mask_soft2 = ~mask_hard2 & (act_result_tbl.TissueStiffness2 > 0);
 
 % - minimum insertion depth mask
-mask_Lmin = act_result_tbl.L >= 30;
-mask_c_1layer = mask_c_1layer & mask_Lmin;
-mask_s_1layer = mask_s_1layer & mask_Lmin;
-mask_c_2layer = mask_c_2layer & mask_Lmin;
+% mask_Lmin = act_result_tbl.L >= 30;
+mask_c_1layer = mask_c_1layer & mask_L;
+mask_s_1layer = mask_s_1layer & mask_L;
+mask_c_2layer = mask_c_2layer & mask_L;
 
 mask_c_2layer_kc2 = mask_c_2layer & (act_result_tbl.kc2_ratio >= 0);
 
@@ -131,31 +131,40 @@ fig = figure(1);
 set(fig, 'Units', 'normalized', 'Position', [0, 0.1, 1,0.8]);
 hold off;
 plot(act_result_tbl.L_ratio(mask_c_1layer & mask_soft1), log(act_result_tbl.kc1_ratio(mask_c_1layer & mask_soft1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '1-Layer C-shape: Soft \kappa_c'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '1-Layer C-shape: Soft \kappa_c'); hold on;
+
+% xlabel('L / L_{ref}'); ylabel('log( \kappa_c / \kappa_{c,ref} )'); 
+% xlim([0, max(xlim)]);
+% legend('location', 'northeastoutside')
+% title("\kappa_c Log-Ratio vs Insertion Depth Ratio");
+% grid on;
+% 
+% figure(2)
+% hold off;
 
 plot(act_result_tbl.L_ratio(mask_c_1layer & mask_hard1), log(act_result_tbl.kc1_ratio(mask_c_1layer & mask_hard1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '1-Layer C-shape: Hard \kappa_c'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '1-Layer C-shape: Hard \kappa_c'); hold on;
 
 
 plot(act_result_tbl.L_ratio(mask_c_2layer & mask_soft1), log(act_result_tbl.kc1_ratio(mask_c_2layer & mask_soft1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '2-Layer C-shape: Soft \kappa_{c,1}'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '2-Layer C-shape: Soft \kappa_{c,1}'); hold on;
 
 plot(act_result_tbl.L_ratio(mask_c_2layer & mask_hard1), log(act_result_tbl.kc1_ratio(mask_c_2layer & mask_hard1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '2-Layer C-shape: Hard \kappa_{c,1}'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '2-Layer C-shape: Hard \kappa_{c,1}'); hold on;
 
 
 plot(act_result_tbl.L_ratio(mask_c_2layer_kc2 & mask_soft2), log(act_result_tbl.kc2_ratio(mask_c_2layer_kc2 & mask_soft2)), ...
-    '.', 'Markersize', 12, 'DisplayName', '2-Layer C-shape: Soft \kappa_{c,2}'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '2-Layer C-shape: Soft \kappa_{c,2}'); hold on;
 
 plot(act_result_tbl.L_ratio(mask_c_2layer_kc2 & mask_hard2), log(act_result_tbl.kc2_ratio(mask_c_2layer_kc2 & mask_hard2)), ...
-    '.', 'Markersize', 12, 'DisplayName', '2-Layer C-shape: Hard \kappa_{c,2}'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '2-Layer C-shape: Hard \kappa_{c,2}'); hold on;
 
 
 plot(act_result_tbl.L_ratio(mask_s_1layer & mask_soft1), log(act_result_tbl.kc1_ratio(mask_s_1layer & mask_soft1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '1-Layer S-shape: Soft \kappa_c'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '1-Layer S-shape: Soft \kappa_c'); hold on;
 
 plot(act_result_tbl.L_ratio(mask_s_1layer & mask_hard1), log(act_result_tbl.kc1_ratio(mask_s_1layer & mask_hard1)), ...
-    '.', 'Markersize', 12, 'DisplayName', '1-Layer S-shape: Hard \kappa_c'); hold on;
+    '.', 'Markersize', 20, 'DisplayName', '1-Layer S-shape: Hard \kappa_c'); hold on;
 
 xlabel('L / L_{ref}'); ylabel('log( \kappa_c / \kappa_{c,ref} )'); 
 xlim([0, max(xlim)]);
@@ -163,7 +172,7 @@ legend('location', 'northeastoutside')
 title("\kappa_c Log-Ratio vs Insertion Depth Ratio");
 grid on;
 
-%% p-value fitting
+% p-value fitting
 fit_soft1 = polyfit(act_result_tbl.L_ratio(mask_c_1layer & mask_soft1), ...
                     log(act_result_tbl.kc1_ratio(mask_c_1layer & mask_soft1)),...
                     1)
@@ -177,5 +186,29 @@ fit_hard1 = polyfit(act_result_tbl.L_ratio(mask_c_1layer & mask_hard1), ...
 p_hard1 = fit_hard1(1)
 p_lsqr_hard1 = act_result_tbl.L_ratio(mask_c_1layer & mask_hard1) \ ...
                log(act_result_tbl.kc1_ratio(mask_c_1layer & mask_hard1))
+
+p_lsqr_all1 = act_result_tbl.L_ratio(mask_c_1layer) \ ...
+              log(act_result_tbl.kc1_ratio(mask_c_1layer))
+
+% entire C-shape insertion (kc1 and kc2)
+L_ratio_cshape = [act_result_tbl.L_ratio(mask_c_1layer);
+                  act_result_tbl.L_ratio(mask_c_2layer);
+                  act_result_tbl.L_ratio(mask_c_2layer)];
+kc_ratio_cshape = [act_result_tbl.kc1_ratio(mask_c_1layer);
+                  act_result_tbl.kc1_ratio(mask_c_2layer);
+                  act_result_tbl.kc2_ratio(mask_c_2layer)];
+
+p_lsqr_cshape = L_ratio_cshape \ log(kc_ratio_cshape)
+
+% plotting
+xl = xlim;
+x = linspace(0, max(act_result_tbl.L_ratio), 100);
+y_soft1 = p_lsqr_soft1 * x;
+y_hard1 = p_lsqr_hard1 * x;
+
+
+plot(x, y_soft1, 'DisplayName', 'Soft \kappa_c fit', 'LineWidth',3);
+plot(x, y_hard1, 'DisplayName', 'Hard \kappa_c fit', 'LineWidth',3);
+
 
 %% Saving
